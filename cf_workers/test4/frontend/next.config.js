22fa30e5268c1fb6c7bab9978c1fba3f6e1d2461
hitgub123 <657578087@qq.com>
2025-08-10 12:09:09 +0900

    1

--- cf_workers/test4/frontend/next.config.js
+++ cf_workers/test4/frontend/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
-  output: 'export',
+  output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
+  // output: 'export',
   images: {
     unoptimized: true,
     domains: ['res.cloudinary.com'],
--- cf_workers/test4/frontend/pages/p/[photoId].tsx
+++ cf_workers/test4/frontend/pages/p/[photoId].tsx
@@ -1,77 +1,109 @@
-import type { GetStaticProps, NextPage } from "next";
-import Head from "next/head";
-import { useRouter } from "next/router";
-import Carousel from "../../components/Carousel";
-import getResults from "../../utils/cachedImages";
-import cloudinary from "../../utils/cloudinary";
-import getBase64ImageUrl from "../../utils/generateBlurPlaceholder";
-import type { ImageProps } from "../../utils/types";
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import cloudinary from '../../utils/cloudinary';
+import getBase64ImageUrl from '../../utils/generateBlurPlaceholder';
+import type { ImageProps } from '../../utils/types';
 
-const Home: NextPage = ({ currentPhoto }: { currentPhoto: ImageProps }) => {
-  const router = useRouter();
-  const { photoId } = router.query;
-  let index = Number(photoId);
-
-  const currentPhotoUrl = `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_2560/${currentPhoto.public_id}.${currentPhoto.format}`;
-
-  return (
-    <>
-      <Head>
-        <title>Next.js Conf 2022 Photos</title>
-        <meta property="og:image" content={currentPhotoUrl} />
-        <meta name="twitter:image" content={currentPhotoUrl} />
-      </Head>
-      <main className="mx-auto max-w-[1960px] p-4">
-        <Carousel currentPhoto={currentPhoto} index={index} />
-      </main>
-    </>
-  );
+const ComicContent: NextPage = ({ images }: { images: ImageProps[] }) => {
+	return (
+		<>
+			<Head>
+				<title>Comic</title>
+			</Head>
+			<main className="mx-auto max-w-[1960px] p-4">
+				<div className="columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
+					{images.map(({ id, public_id, format, blurDataUrl }) => (
+						<Link
+							key={id}
+							href={`/p/${id}`}
+							className="after:content after:shadow-highlight group relative mb-5 block w-full cursor-pointer after:pointer-events-none after:absolute after:inset-0 after:rounded-lg"
+						>
+							<Image
+								alt="Next.js Conf photo"
+								className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
+								style={{ transform: 'translate3d(0, 0, 0)' }}
+								placeholder="blur"
+								blurDataURL={blurDataUrl}
+								src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${public_id}.${format}`}
+								width={720}
+								height={480}
+								sizes="(max-width: 640px) 100vw,
+                  (max-width: 1280px) 50vw,
+                  (max-width: 1536px) 33vw,
+                  25vw"
+							/>
+						</Link>
+					))}
+				</div>
+			</main>
+			<footer className="p-6 text-center text-white/80 sm:p-12">Thank you for watching the pictures.</footer>
+		</>
+	);
 };
 
-export default Home;
+export default ComicContent;
 
-export const getStaticProps: GetStaticProps = async (context) => {
-  const results = await getResults();
+export async function getStaticProps(context: any) {
+	let index = context.params.photoId;
+	const results = await cloudinary.v2.search
+		.expression(`folder:${process.env.CLOUDINARY_FOLDER}/${index}`)
+		// .expression(`folder:={process.env.CLOUDINARY_FOLDER}`)
+		.sort_by('public_id', 'desc')
+		.max_results(10)
+		.execute();
+	let reducedResults: ImageProps[] = [];
 
-  let reducedResults: ImageProps[] = [];
-  let i = 0;
-  for (let result of results.resources) {
-    reducedResults.push({
-      id: i,
-      height: result.height,
-      width: result.width,
-      public_id: result.public_id,
-      format: result.format,
-    });
-    i++;
-  }
+	let i = 0;
+	for (let result of results.resources) {
+		reducedResults.push({
+			id: i,
+			height: result.height,
+			width: result.width,
+			public_id: result.public_id,
+			format: result.format,
+		});
+		i++;
+	}
 
-  const currentPhoto = reducedResults.find(
-    (img) => img.id === Number(context.params.photoId),
-  );
-  currentPhoto.blurDataUrl = await getBase64ImageUrl(currentPhoto);
+	const blurImagePromises = results.resources.map((image: ImageProps) => {
+		return getBase64ImageUrl(image);
+	});
+	const imagesWithBlurDataUrls = await Promise.all(blurImagePromises);
 
-  return {
-    props: {
-      currentPhoto: currentPhoto,
-    },
-  };
-};
+	for (let i = 0; i < reducedResults.length; i++) {
+		reducedResults[i].blurDataUrl = imagesWithBlurDataUrls[i];
+	}
+
+	return {
+		props: {
+			images: reducedResults,
+		},
+	};
+}
 
 export async function getStaticPaths() {
-  const results = await cloudinary.v2.search
-    .expression(`folder:${process.env.CLOUDINARY_FOLDER}/*`)
-    .sort_by("public_id", "desc")
-    .max_results(400)
-    .execute();
+	const results = await cloudinary.v2.api.sub_folders(process.env.CLOUDINARY_FOLDER);
+	const folders = results.folders;
+	// console.log(results)
 
-  let fullPaths = [];
-  for (let i = 0; i < results.resources.length; i++) {
-    fullPaths.push({ params: { photoId: i.toString() } });
-  }
-
-  return {
-    paths: fullPaths,
-    fallback: false,
-  };
+	let fullPaths = [];
+	for (let i = 0; i < folders.length; i++) {
+		fullPaths.push({ params: { photoId: folders[i].name } });
+	}
+	console.log(fullPaths)
+	return {
+		paths: fullPaths,
+		fallback: false,
+	};
+	// return {
+	// 	// paths:[],
+	// 	paths: [
+	// 		{ params: { photoId: '2' } },
+	// 		{ params: { photoId: '3' } },
+	// 	],
+	// 	fallback: false,
+	// };
 }
